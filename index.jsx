ee74c93e6ecfe4da31235813f0786b4c9e08ca41
David Mendes <david.mendes@mindswap.academy>
2022-10-31 15:12:22 +0000

    starting comment section with react

--- index.jsx
+++ index.jsx
@@ -1,8 +1,13 @@
 const commentForm = (
-
-    <form>name</form>,
-    <textarea className="commentArea">write here</textarea>
- 
+    <div>
+        <form>
+            <label htmlFor="commentName">Name:</label><br/>
+            <input type="text" id="commentName" className="formNameClass"/><br/>
+            <label htmlFor="textAreaId">Comment:</label><br/>
+            <textarea id="textAreaId" className="commentArea"></textarea><br/>
+            <button>Submit</button>
+        </form> 
+    </div>
 );
 
 ReactDOM.render(commentForm , document.getElementById("reactDiv"));
