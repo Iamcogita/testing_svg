02c73cb1df7d784d53c3300499fcd401e5192002
David Mendes <david.mendes@mindswap.academy>
2022-11-02 17:57:07 +0000

    wip**

--- index.jsx
+++ index.jsx
@@ -1,12 +1,24 @@
-const commentForm = (
-    <div><h3>LEAVE A COMMENT</h3><hr/>
+const MyCommentSection = () => {
+    return(
+    <div id="commentDivId"><h3>LEAVE A COMMENT</h3><hr/>
         <form id="formID">
             <label htmlFor="commentName">Name:</label><br/>
-            <input type="text" id="commentName" className="formNameClass"/><br/>
+            <input type="text" id="commentName" className="formNameClass" placeholder="your name"/><br/>
             <label htmlFor="textAreaId">Comment:</label><br/>
-            <textarea id="textAreaId" className="commentArea"></textarea><br/>
-            <button id="submitB">Submit</button>
-        </form> 
-    </div>
-);
-ReactDOM.render(commentForm , document.getElementById("reactDiv"));
+            <textarea id="textAreaId" className="commentArea" placeholder="write here"></textarea><br/>
+            <button id="submitButton" onClick={handleSubmit}> Submit </button>
+        </form>
+        <div id="databaseComments">
+        </div>
+    </div> 
+    )
+}
+
+const handleSubmit = () => {
+
+}
+
+
+const rootNode = document.getElementById("reactDiv");
+const root = ReactDOM.createRoot(rootNode);
+root.render(<MyCommentSection/>);
